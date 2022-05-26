a5620334a0a5d227c62be044ed5d1f81e38621c5
eric-freitas <eric.freitas.matos@gmail.com>
2022-05-26 20:26:39 -0300

    de novo...

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,7 +7,7 @@ i18n
 	.use(initReactI18next)
 	.init({
 		backend: {
-			loadPath: 'sample-web/locales/{{lng}}/{{ns}}.json'
+			loadPath: './locales/{{lng}}/{{ns}}.json'
 		},
 		fallbackLng: 'es',
 		debug: false,
