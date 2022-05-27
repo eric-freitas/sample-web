69d2378a77dac2ff2f3849ba7de310ca45802edf
eric-freitas <eric.freitas.matos@gmail.com>
2022-05-27 12:06:38 -0300

    slash icon

--- src/components/Icons/Sections/Slash/index.tsx
+++ src/components/Icons/Sections/Slash/index.tsx
@@ -17,7 +17,7 @@ export default function SlashIcon(props: SlashIconProps) {
             /> 
             <mask id="slash-mask">
                 <rect x="0" y="0" width={width} height={height} fill="white" />
-                <path fill="black" d="m 235.6876,243.8327 c 1.1857,1.1557 2.7128,1.7552 4.3719,1.6787 l -0.5593,0.2488 H 205.0349 L 1e-4,40.7255 V 6.2412 L 0.355,4.8565 c -0.3685,1.8275 0.1557,3.7992 1.5724,5.216 77.9154,77.9153 155.8398,155.8397 233.7602,233.7602 z"/>
+                <path fill="black" d="M 1e-4,7.9403 237.8173,245.7602 H 205.2339 L 1e-4,40.5237 Z"/>
             </mask>
         </g>
     );
